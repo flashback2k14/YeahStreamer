046fff61aab13726b79054b11e707936f916a058
Sebastian Kloppe <feedbackcalcest@gmail.com>
2016-03-07 14:12:26 +0100

    add crashlytics

--- app/src/main/java/com/yeahdev/yeahstreamer/AppApplication.java
+++ app/src/main/java/com/yeahdev/yeahstreamer/AppApplication.java
@@ -2,6 +2,8 @@ package com.yeahdev.yeahstreamer;
 
 import android.app.Application;
 import com.firebase.client.Firebase;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 
 public class AppApplication extends Application {
@@ -10,5 +12,6 @@ public class AppApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Firebase.setAndroidContext(this);
+        Fabric.with(this, new Crashlytics());
     }
 }
